2351596f28d638f5a65374390b8977deeaaf7d4e
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-11 21:24:10 +0100

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import logoSponsor from './images/adalab.png';
+
+function Footer() {
+
+    return (
+
+        <footer className="footer">
+            <img className="logoSponsor" src={logoSponsor} alt="Logo Adalab" />
+        </footer>
+
+    );
+
+}
+
+export default Footer;
+
+
+
+
