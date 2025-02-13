6b032528c59ce283eaeeb226d709bf0dd26b86bf
Julia <julialopezcalvo95@gmail.com>
2025-02-13 21:12:31 +0100

    landin sass

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -10,7 +10,7 @@ function Landing() {
 
                 <img className="graphic" src={Graphic} alt="adalab-graphic" />
                 <h1 className="titleh1">Proyectos molones</h1>
-                <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
+                <p className="landing__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
                 <Link to="/create" className="button--link" >Nuevo Proyecto</Link>
             </section>
         </>
