c5bcfa13b56a81abee93504aafa323c676942dab
Julia <julialopezcalvo95@gmail.com>
2025-02-13 20:35:19 +0100

    create Landing

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 function Landing() {
 
@@ -5,9 +6,9 @@ function Landing() {
         <>
 
 <section className="hero">
-            <h2 className="title">Proyectos molones</h2>
+            <h1 className="title">Proyectos molones</h1>
             <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
-            <a className="button--link" href="./">Ver proyectos</a>
+            <Link to="/create" className="button--link" >Nuevo Proyecto</Link>
         </section>
         </>
     )
