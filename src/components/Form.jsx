37fe8849d105c6d906521ed2c39ccc568ec0737e
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-14 21:40:38 +0100

    Fix styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 function Form({ projectData, setProjectData }) {
 
-    const handleFileChange = (event, field) => {
-        const file = event.target.files[0];
+    const handleFileChange = (ev, field) => {
+        const file = ev.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
@@ -21,43 +21,43 @@ function Form({ projectData, setProjectData }) {
                 <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
 
                 <input className="addForm__input" type="text" name="name" placeholder="Nombre del proyecto"
-                    value={projectData.name} onChange={(e) => setProjectData({ ...projectData, name: e.target.value })} />
+                    value={projectData.name} onChange={(ev) => setProjectData({ ...projectData, name: ev.target.value })} />
 
                 <input className="addForm__input" type="text" name="slogan" placeholder="Slogan"
-                    value={projectData.slogan} onChange={(e) => setProjectData({ ...projectData, slogan: e.target.value })} />
+                    value={projectData.slogan} onChange={(ev) => setProjectData({ ...projectData, slogan: ev.target.value })} />
 
                 <input className="addForm__input" type="text" name="technologies" placeholder="Tecnologías"
-                    value={projectData.technologies} onChange={(e) => setProjectData({ ...projectData, technologies: e.target.value })} />
+                    value={projectData.technologies} onChange={(ev) => setProjectData({ ...projectData, technologies: ev.target.value })} />
 
                 <input className="addForm__input" type="url" name="repo" placeholder="Repositorio"
-                    value={projectData.repo} onChange={(e) => setProjectData({ ...projectData, repo: e.target.value })} />
+                    value={projectData.repo} onChange={(ev) => setProjectData({ ...projectData, repo: ev.target.value })} />
 
                 <input className="addForm__input" type="url" name="demo" placeholder="Demo"
-                    value={projectData.demo} onChange={(e) => setProjectData({ ...projectData, demo: e.target.value })} />
+                    value={projectData.demo} onChange={(ev) => setProjectData({ ...projectData, demo: ev.target.value })} />
 
                 <textarea className="addForm__input" name="desc" placeholder="Descripción" rows="5"
-                    value={projectData.desc} onChange={(e) => setProjectData({ ...projectData, desc: e.target.value })}></textarea>
+                    value={projectData.desc} onChange={(ev) => setProjectData({ ...projectData, desc: ev.target.value })}></textarea>
             </fieldset>
 
             <fieldset className="addForm__group">
                 <legend className="addForm__title">Cuéntanos sobre la autora</legend>
 
                 <input className="addForm__input" type="text" name="autor" placeholder="Nombre"
-                    value={projectData.autor} onChange={(e) => setProjectData({ ...projectData, autor: e.target.value })} />
+                    value={projectData.autor} onChange={(ev) => setProjectData({ ...projectData, autor: ev.target.value })} />
 
                 <input className="addForm__input" type="text" name="job" placeholder="Trabajo"
-                    value={projectData.job} onChange={(e) => setProjectData({ ...projectData, job: e.target.value })} />
+                    value={projectData.job} onChange={(ev) => setProjectData({ ...projectData, job: ev.target.value })} />
             </fieldset>
 
             <fieldset className="addForm__group--upload">
                 <label className="button">
                     Subir foto del proyecto
-                    <input className="addForm__hidden" type="file" onChange={(e) => handleFileChange(e, "photo")} />
+                    <input className="addForm__hidden" type="file" onChange={(ev) => handleFileChange(ev, "photo")} />
                 </label>
 
                 <label className="button">
                     Subir foto de la autora
-                    <input className="addForm__hidden" type="file" onChange={(e) => handleFileChange(e, "image")} />
+                    <input className="addForm__hidden" type="file" onChange={(ev) => handleFileChange(ev, "image")} />
                 </label>
 
                 <button className="button--large">Guardar proyecto</button>
